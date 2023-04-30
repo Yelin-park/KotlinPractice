a2359a00dd06a37f851f404ecc90024c3cdc7715
yaliny <mae1217@naver.com>
2023-04-30 23:24:33 +0900

    코틀린 200제 정수 타입과 실수 타입, 실수 타입의 함정 타입 별명(typealias) 문자와 문자열, 문장, 주석 문자열 안에 표현식의 값을 집어넣기($, ${}) 배정 연산자, 논리 연산자, 비교 연산자 흐름 제어문(if, if-else, 중첩) : if-else로 변수에 값을 할당 가능(삼항연산자 사용 불가능)

---
+++ src/main/kotlin/Part1/TypeAlias.kt
@@ -0,0 +1,15 @@
+package Part1
+
+import Part1.Number
+
+/*
+[타입 별명]
+    typealias 라는 키워드를 사용하면 이미 존재하는 타입에 별명을 붙일 수 있음
+ */
+typealias Number = Int
+
+fun main(args: Array<String>): Unit
+{
+    val a: Number = 10
+    println(a)
+}
